d51ade29e06a59e6f5fa130cd4d6fc9334559804
Sahil Lather <sahillather002@gmail.com>
2023-07-26 20:02:44 +0530

    maze map function

--- aStar.py
+++ aStar.py
@@ -1,5 +1,12 @@
 from pyamaze import maze
+#defining maze size
 m=maze(5,5)
 m.CreateMaze()
+
+# print(m.maze_map)
+# mapping is done in four directions e w n s
+# e.g : (1,1): e=1 w=0 n=0 s=0
+# 1 means path to that direction is open but path to 0 is closed 
+
 
 m.run()
